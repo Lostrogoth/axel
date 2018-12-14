8199068be1eb5a15a5d3d6c40ed1921c7dbc19ed
LucineGx <lgaveria@student.42.fr>
2018-12-14 10:27:35 +0100

    Add files via upload

---
+++ vrac_fonctions_appel_ajax.js
@@ -0,0 +1,76 @@
+// REMPLIR LES PATH
+
+<script>
+
+	var sessionId = 0;
+
+	var lat = 0;
+	var long = 0;
+
+	$(function() {
+
+	// Mettons que le nom de ton boutton c'est startBtn
+		$('#startBtn').click(function() {
+
+			receiveNewSessionId();
+
+			setInterval(sendLocationData(), 10000);
+		});
+
+	// quand l'user click sur GO
+		function receiveNewSessionId() {
+
+			$.ajax({
+				url: "/path/createNewSessionId",
+				success: function(data) {
+		
+					if (data && data > 0)
+						sessionId = data;
+					else{
+						newIdFailed();
+						console.log("no data - data = "+data);
+					}
+				},
+				error: function(data) {
+					newIdFailed();
+					console.log("l'appel de la fonction 'createNewSessionId' a échoué.");
+					console.log(data);
+				}
+			})
+		}
+
+		function newIdFailed() {
+
+		}
+
+	// appelée toutes les 10 secondes après GO
+		function sendLocationData() {
+
+		// enregistrer dans les variables long et lat, à ce moment là
+			getLocation();
+
+			var now = new Date();
+			var date = now.getDate();
+
+			$.ajax({
+				url: '/path/getLocationData',
+				data: { id:sessionId, jour:date, long:long, lat:lat },
+				type: 'post',
+				success: function(){
+					console.log("success");
+				},
+				error : function(data) {
+					sendLocationFailed();
+					console.log("l'appel de la fonction 'getLocationData' a échoué.");
+					console.log(data);
+				}
+			});
+		}
+
+		function sendLocationFailed() {
+
+		}
+
+	});
+
+</script>
